1e477b8e236f9be42898735ea5fb66a0c56e69d4
manikanta Routhu <manikantar129@gmail.com>
2026-06-03 18:13:01 +0530

    eks cluster

---
+++ vars/pipelineGlobals.groovy
@@ -0,0 +1,9 @@
+static final String region(){
+    return "us-east-1"
+}
+static final Stage nexusUrl(){
+    return  "nexus.daws.info:8081"
+}
+static final String account_id(){
+    return  "222634371492"
+}
